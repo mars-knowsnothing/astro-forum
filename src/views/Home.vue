1ef4ce21b5ff1a861eb1bd5a805c8ff5b4345d86
RUORANL <ruoran.li@mercedes-benz.com>
2024-10-15 17:22:23 +0800

    1. update code to display email right

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,31 @@
         {{ paragraph }}
       </p>
       <p class="text-justify text-lg font-bold leading-relaxed">{{ $t('home.list_1_title') }}</p>
-      <p class="text-justify text-lg leading-relaxed">{{ $t('home.list_1') }}</p>
+      <div v-for="(item, index) in list_1" :key="index">
+        <!-- 如果 item 是字符串，显示为段落 -->
+        <p v-if="!isList(item)" class="text-justify text-lg">
+          <span v-html="itemWithLinks(item)"></span>
+        </p>
+        <!-- 如果 item 是列表，显示为无序列表 -->
+        <ul v-else class="list-disc list-inside space-y-2">
+          <li v-for="(listItem, idx) in item" :key="idx">
+            <span v-html="itemWithLinks(listItem)"></span>
+          </li>
+        </ul>
+      </div>
       <p class="text-justify text-lg font-bold leading-relaxed">{{ $t('home.list_2_title') }}</p>
-      <p class="text-justify text-lg leading-relaxed">{{ $t('home.list_2') }}</p>
+      <div v-for="(item, index) in list_2" :key="index">
+        <!-- 如果 item 是字符串，显示为段落 -->
+        <p v-if="!isList(item)" class="text-justify text-lg">
+          <span v-html="itemWithLinks(item)"></span>
+        </p>
+        <!-- 如果 item 是列表，显示为无序列表 -->
+        <ul v-else class="list-disc list-inside space-y-2">
+          <li v-for="(listItem, idx) in item" :key="idx">
+            <span v-html="itemWithLinks(listItem)"></span>
+          </li>
+        </ul>
+      </div>
     </div>   
     <!-- <h4 class="text-xl font-bold mt-8">{{ $t('home.section_1_title') }}</h4>
     <div class="mt-8 space-y-6">
@@ -52,8 +74,37 @@ export default {
     },
     section_2_content() {
       return this.$i18n.messages[this.$i18n.locale].home.section_2_content;
+    },
+    list_1() {
+      return this.$i18n.messages[this.$i18n.locale].home.list_1;
+    },
+    list_2() {
+      return this.$i18n.messages[this.$i18n.locale].home.list_2;
     }
   },
+  methods: {
+    isList(item) {
+      return Array.isArray(item);
+    },
+    itemWithLinks(item) {
+      // 检测并转换网址和邮箱为可点击链接
+      let text = item;
+
+      // 转换邮箱为邮件链接
+      const emailRegex = /([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,})/g;
+      text = text.replace(emailRegex, (email) => {
+        return `<a href="mailto:${email}" class="text-blue-500 underline">${email}</a>`;
+      });
+
+      // 转换 http 开头的 URL 为链接
+      const urlRegex = /(http[s]?:\/\/[^\s]+)/g;
+      text = text.replace(urlRegex, (url) => {
+        return `<a href="${url}" target="_blank" class="text-blue-500 underline">${url}</a>`;
+      });
+      
+      return text;
+    },
+  }
 };
 </script>
 
