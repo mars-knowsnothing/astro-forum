7c8a44cad29a929d1ad218b6386e7f40eefd1fa7
RUORANL <ruoran.li@mercedes-benz.com>
2024-10-19 09:22:33 +0800

    1. update content

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -13,7 +13,7 @@ const messages = {
       registration: 'Registration',
     },
     home: {
-      title: 'Announcement of 2024 PKU International PhD Student Forum on the Frontiers of Modern Astronomy',
+      title: 'KIAA Reasearch Forum for Postdoctoral Scholars in Astronomy and Astrophysics',
       content: [
         'We are happy to announce the KIAA Postdoc Forum 2024, to be held in the Kavli Institute for Astronomy and Astrophysics at Peking University from November 25th to November 27th 2024. The forum is aimed at, but not restricted to, postdocs currently based in China or interested in pursuing an academic career here. Other early-career scientists are also welcome, both from China and abroad.',
         'We welcome contributions from a broad range of topics in theoretical and observational Astrophysics: from Stellar and Galaxy evolution to General Relativity and Cosmology. Our main goal is to foster networking and collaboration opportunities among participants, so a significant part of the forum will be devoted to open discussion and interaction. We will also ensure that every participant has time allotted to showcase their research.',
@@ -21,31 +21,31 @@ const messages = {
       ],
       list_1_title: 'Scientific Organizing Committee:',
       list_1: [
-        'Pablo Renard Guiral (p.renard.guiral@gmail.com)',
-        'Sunayana Maben (smaben@nao.cas.cn)',
-        'Hsu-Wen Chiang (jiangxw@sustech.edu.cn)',
-        'Huan Zhou (zhouh237@mail3.sysu.edu)',
-        'Yun-Feng Wei (yfwei@xmu.edu.cn)',
-        'Yun Wang (wangyun@pmo.ac.cn)'
+        'Pablo Renard Guiral (p.renard.guiral AT gmail.com)',
+        'Sunayana Maben (smaben AT nao.cas.cn)',
+        'Hsu-Wen Chiang (jiangxw AT sustech.edu.cn)',
+        'Huan Zhou (zhouh237 AT mail3.sysu.edu)',
+        'Yun-Feng Wei (yfwei AT xmu.edu.cn)',
+        'Yun Wang (wangyun AT pmo.ac.cn)'
       ],
       bannerText: [
-        'KIAA Astronomy and Astrophysics Postdoc Forum 2024',
+        'KIAA Reasearch Forum for Postdoctoral Scholars in Astronomy and Astrophysics',
         'November 25 - 27 , 2024'
       ],
       list_2_title: 'Local Organizing Committee:',
       list_2: [
-        'Veronica Vazquez Aceves',
-        'Haonan Zheng',
-        'Torben C. Frost',
-        'Siqi Liu',
-        'Meng-Hua Chen'
+        'Veronica Vazquez Aceves, KIAA postdoc (veronica AT pku.edu.cn)',
+        'Haonan Zheng, KIAA postdoc (hzzheng AT pku.edu.cn)',
+        'Torben C. Frost, KIAA postdoc (torben.frost AT pku.edu.cn)',
+        'Siqi Liu, KIAA postdoc (liusiqi_647 AT pku.edu.cn)',
+        'Meng-Hua Chen, KIAA postdoc (physcmh AT pku.edu.cn)'
       ]
     },
     registration: {
       title: 'Registration',
       content: [
         'Registration for the 2024 PKU PostDocs Forum',
-        ['Forum Homepage:', '<需要确认域名>'],
+        ['Forum Homepage:', 'https://postdoc-forum-2024.kiaa-pku.cn'],
         'Forum Venue: KIAA-PKU Auditorium',
         'Forum Dates: November 25th to November 27th, 2024',
         'Registration Period: <需要确认注册时间>',
@@ -57,13 +57,9 @@ const messages = {
       title: 'Contact Information',
       content: [
         'All information regarding the forum can be found on the website:',
-        ['https://phd-forum-2024.kiaa-pku.cn/'],
+        ['https://postdoc-forum-2024.kiaa-pku.cn/'],
         'If you have any questions, or need visa application support, please contact: ',
         // 将联系人信息作为数组
-        [
-          'Ms. Sun Min, Email: minsun1206@pku.edu.cn',
-          'Ms. Xie Jing, Email: xiejing5166@pku.edu.cn',
-        ],
         'We are looking forward to meeting you at 2024 PKU International PhD Student Forum on the Frontiers of Modern Astronomy!',
       ],
     },
@@ -103,38 +99,44 @@ const messages = {
       registration: '注册方式',
     },
     home: {
-      title: '2024 年北京大学现代天文学前沿国际博士生论坛通知',
+      title: '科维里天文与天体物理博士后论坛',
       content: [
-        '作为北京大学“学科质量年”活动之一，北京大学诚邀天文学和天体物理学专业的研究生参加将于2024年12月5日至10日在北京大学科维理天文与天体物理研究所（KIAA-PKU）举办的2024年北京大学现代天文学前沿国际博士生论坛。',
-        '近年来，中国天文学发展迅速，在射电（FAST、TianMa、QTT）和光学（LAMOST、WFST、CSST）以及高能天体物理（DAMPE、HXMT、LHAASO、EP、SVOM）等领域的大批先进的地面和空间设施已经建成或正在建设中。本次论坛将涵盖射电天文学、太阳与行星、恒星与银河系、高能天体物理、星系与宇宙学等主题，重点关注相关设施取得的最新科学进展，并讨论如何有效参与其中的科学研究。论坛还将为学生提供展示和交流自己研究成果的机会。',
-        '北京大学欢迎国内外的研究生参加本次论坛。所有讲座和讨论都将使用英语。本论坛由北京大学物理学院天文学系和科维理天文与天体物理研究所组织，旨在促进不同国家和地区天文学科研究生之间的学术交流，培养学生开展天文学和天体物理学前沿科学研究。',
-        '欢迎各国和各地区的天文学专业研究生参加，鼓励所有学生在学习报告内容的同时展示自己的研究成果。',
-        '我们期待在2024年北京大学现代天文学前沿国际博士生论坛上与您相见！'
+        '我们很高兴宣布2024年KIAA博士后论坛将于2024年11月25日至11月27日在北京大学卡弗里天文与天体物理研究所举行。本次论坛主要面向目前在中国的博士后研究人员或有意在中国发展学术事业的博士后，但不限于此。其他早期职业阶段的科学家，无论是来自中国还是海外，也同样欢迎参加。',
+        '我们欢迎来自理论和观测天文学广泛领域的贡献：从恒星与星系演化到广义相对论和宇宙学。我们的主要目标是促进参与者之间的网络和合作机会，因此论坛的相当一部分时间将专门用于开放讨论和互动交流。此外，我们还将确保每位参与者都有机会展示他们的研究。',
+        '在过去十年中，中国的天文学发展迅速，新观测仪器和设施正在规划或已投入使用（例如FAST、MUST、CSST、天琴、太极），并且获得的资助机会也在增加。KIAA博士后论坛为早期职业科学家提供了在中国扩大科学网络的机会，展望未来，他们可以继续在中国发展科学事业或与中国机构进一步合作。'
       ],
       list_1_title: '科学组织委员会：',
-      list_1: 'Gregory Herczeg（沈雷歌）, Luis C. Ho（何子山）, 江林华, 黎卓, 吴学兵 (主席), 张华伟',
+      list_1: [
+        'Pablo Renard Guiral (p.renard.guiral AT gmail.com)',
+        'Sunayana Maben (smaben AT nao.cas.cn)',
+        'Hsu-Wen Chiang (jiangxw AT sustech.edu.cn)',
+        'Huan Zhou (zhouh237 AT mail3.sysu.edu)',
+        'Yun-Feng Wei (yfwei AT xmu.edu.cn)',
+        'Yun Wang (wangyun AT pmo.ac.cn)'
+      ],
       list_2_title: '地方组织委员会：',
-      list_2: '刘黎黎, 刘树岩(主席), 孙敏, 谢静, 姚洁, 张硕',
-      bannerText: ['2024 年北京大学现代天文学前沿国际博士生论坛'],
+      list_2: [
+        'Veronica Vazquez Aceves, KIAA postdoc (veronica AT pku.edu.cn)',
+        'Haonan Zheng, KIAA postdoc (hzzheng AT pku.edu.cn)',
+        'Torben C. Frost, KIAA postdoc (torben.frost AT pku.edu.cn)',
+        'Siqi Liu, KIAA postdoc (liusiqi_647 AT pku.edu.cn)',
+        'Meng-Hua Chen, KIAA postdoc (physcmh AT pku.edu.cn)'
+      ],
+      bannerText: ['科维里天文与天体物理博士后论坛','2024年11月25日 至 11月27日'],
     },
     schedule: {
       subTitle: '初步日程',
       schedule: [
-        '12月4日   报到',
-        '12月5日   星系与宇宙学',
-        '12月6日   高能天体物理',
-        '12月7日   射电天文',
-        '12月8日   恒星与银河系',
-        '12月9日   太阳与行星',
-        '12月10日  离会'
+        '11月25日（星期一）：特邀报告、讨论、海报展示会',
+        '11月26日（星期二）：特邀报告、讨论、海报展示会',
+        '11月27日（星期三）：特邀报告、讨论、海报展示会',
       ]
     },
     registration: {
-      title: '注册方式',
+      title: '注册',
       content: [
-        '2024 年北京大学现代天文学前沿国际博士生论坛的注册将从 9 月 20 日开放至 10 月 25 日或报名名额招满为止，限额 100 名学生。参与者应注明他们是否希望进行口头报告或海报展示。被接受口头报告的同学将于 2024 年 11 月 1 日前收到通知。',
-        '本次论坛不收取注册费。交通和住宿费用自理。论坛将为所有参会者提供每日午餐和晚餐。',
-        ['论坛主页：', 'https://phd-forum-2024.kiaa-pku.cn/'],
+        '<需要补充中文内容>',
+        ['论坛主页：', 'https://postdoc-forum-2024.kiaa-pku.cn/'],
         '论坛地点：北京大学科维理天文与天体物理研究所报告厅',
         '论坛日期：2024 年 12 月 5 日至 12 月 10 日',
         '注册时间：2024 年 9 月 20 日至 2024 年 10 月 25 日（100名，额满即止）',
